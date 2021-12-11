84e42b99ed902462b0699541177470e45c273843
Carbon225 <carbon225@protonmail.com>
2021-12-12 00:34:48 +0100

    Segment

--- src/sweeping.jl
+++ src/sweeping.jl
@@ -7,16 +7,37 @@ using DataStructures
 
 
 
+# Segment
+
+struct Segment{T}
+    slope::T
+    intercept::T
+    line::Line{2, T}
+    x1::T
+    x2::T
+end
+
+function Segment(l::Line{2, T}) where T
+    p1, p2 = line
+    x1, y1 = p1
+    x2, y2 = p2
+    slope = (y2 - y1) / (x2 - x1)
+    intercept = y1 - slope * x1
+    Segment{T}(slope, intercept, l, min(l[1][1], l[2][1]), max(l[1][1], l[2][1]))
+end
+
+
+
 # State
 
-struct State
+struct State{T}
 
 end
 
-Base.delete!(st::State, s) = throw("Not implemented")
-pred(st::State, s) = throw("Not implemented")
-succ(st::State, s) = throw("Not implemented")
-flip!(st::State, s1, s2) = throw("Not implemented")
+Base.delete!(st::State{T}, s::Segment{T}) where T = throw("Not implemented")
+pred(st::State{T}, s::Segment{T}) where T = throw("Not implemented")
+succ(st::State{T}, s::Segment{T}) where T = throw("Not implemented")
+flip!(st::State{T}, s1::Segment{T}, s2::Segment{T}) where T = throw("Not implemented")
 
 
 
@@ -28,12 +49,12 @@ abstract type AbstractEvent end
 
 # Begin Event
 
-struct BeginEvent <: AbstractEvent
-
+struct BeginEvent{T} <: AbstractEvent
+    s::Segment{T}
 end
 
-getsegment(ev::BeginEvent) = throw("Not implemented")
-getpriority(ev::BeginEvent) = throw("Not implemented")
+getsegment(ev::BeginEvent) = ev.s
+getpriority(ev::BeginEvent) = ev.s.x1
 
 
 
@@ -43,19 +64,21 @@ struct EndEvent <: AbstractEvent
 
 end
 
-getsegment(ev::EndEvent) = throw("Not implemented")
-getpriority(ev::EndEvent) = throw("Not implemented")
+getsegment(ev::EndEvent) = ev.s
+getpriority(ev::EndEvent) = ev.s.x2
 
 
 
 # IntersectionEvent
 
-struct IntersectionEvent <: AbstractEvent
-
+struct IntersectionEvent{T} <: AbstractEvent
+    s1::Segment{T}
+    s2::Segment{T}
+    intersectionx::T
 end
 
-getsegments(ev::IntersectionEvent) = throw("Not implemented")
-getpriority(ev::IntersectionEvent) = throw("Not implemented")
+getsegments(ev::IntersectionEvent) = (ev.s1, ev.s2)
+getpriority(ev::IntersectionEvent) = ev.intersectionx
 
 
 
@@ -70,9 +93,9 @@ function Events(lines::Vector{Line{2, T}}) where T
 end
 
 Base.isempty(eq::Events) = isempty(eq.q)
-Base.push!(eq::Events, ev::E) where E<:AbstractEvent = enqueue!(eq.q, ev, getpriority(ev))
+Base.push!(eq::Events{T}, ev::E{T}) where T, E<:AbstractEvent = enqueue!(eq.q, ev, getpriority(ev))
 Base.pop!(eq::Events) = dequeue!(eq.q)
-checkintersection!(eq::Events, s1, s2) = throw("Not implemented")
+checkintersection!(eq::Events{T}, s1::Segment{T}, s2::Segment{T}) where T = throw("Not implemented")
 
 
 function findintersections(lines::Vector{Line{2, T}}) where T
