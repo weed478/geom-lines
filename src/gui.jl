a58f6c550551b5d79114d098f720496a333f434d
Carbon225 <carbon225@protonmail.com>
2021-12-11 20:42:19 +0100

    basic points

--- src/gui.jl
+++ src/gui.jl
@@ -1,7 +1,70 @@
 module gui
 
+using GLMakie
+using GeometryBasics
+
 function run()
-    println("Hello world")
+    fig = Figure()
+    fig[2, 1] = controlsgrid = GridLayout(tellwidth=false)
+    ax = Axis(fig[1, 1])
+    deregister_interaction!(ax, :rectanglezoom)
+
+    points = Node(Point2f[])
+
+    scatter!(
+        ax,
+        points,
+    )
+
+    function pushpoint!(p)
+        points[] = push!(points[], p)
+    end
+
+    function poppoint!()
+        length(points[]) > 0 && pop!(points[])
+        points[] = points[]
+    end
+
+    function clearpoints!()
+        points[] = empty(points[])
+    end
+
+    mouseevents = addmouseevents!(ax.scene)
+    onmouseleftdown(mouseevents) do event
+        dpos = event.data
+        pushpoint!(dpos)
+    end
+
+    on(events(fig).keyboardbutton) do event
+        if event.action == Keyboard.press
+            if event.key == Keyboard.r
+                clearpoints!()
+            elseif event.key == Keyboard.a
+                autolimits!(ax)
+            elseif event.key == Keyboard.p
+                poppoint!()
+            end
+        end
+    end
+
+    resetbtn = controlsgrid[1, 1] = Button(fig, label="Reset (R)")
+    on(resetbtn.clicks) do n
+        clearpoints!()
+    end
+
+    autoscalebtn = controlsgrid[1, 2] = Button(fig, label="Autoscale (A)")
+    on(autoscalebtn.clicks) do n
+        autolimits!(ax)
+    end
+
+    popbtn = controlsgrid[1, 3] = Button(fig, label="Remove last (P)")
+    on(popbtn.clicks) do n
+        poppoint!()
+    end
+
+    display(fig)
+
+    nothing
 end
 
 end
